6441af493b8c70ae6d2e798ff4032b48d5d94235
renatoseb <renatorodriguezllanos@gmail.com>
2022-05-27 15:54:37 -0500

    aun no sale

--- main.cpp
+++ main.cpp
@@ -6,16 +6,18 @@
 // forward: input * primera matriz y backward(input)
 using namespace boost::numeric::ublas;
 
-matrix<double> getMatrix(int nRows, int nCols)
+matrix<double> getMatrix(int &nRows, int &nCols)
 {
     std::random_device rd;
     std::default_random_engine eng(rd());
     std::uniform_real_distribution<double> distr(0, 10);
 
     matrix<double> init(nRows, nCols);
-    for(size_t i=0; i<init.size1(); i++) {
-        for(size_t j=0; j<init.size2(); j++) {
-            init(i,j) = distr(eng);
+    for (size_t i = 0; i < nRows; i++)
+    {
+        for (size_t j = 0; j < nCols; j++)
+        {
+            init(i, j) = distr(eng);
         }
     }
     return init;
@@ -28,92 +30,114 @@ private:
     std::vector<int> nodosh;
     int output;
     std::vector<matrix<double>> matrices;
-    
-public:
-    MLP(std::vector<double> input, std::vector<int> nodosh) {
 
+public:
+    MLP(std::vector<double> &input, std::vector<int> &nodosh,int &ouput)
+    {
+        std::cout << "entrando a constructor\n";
         this->input = input;
         this->nodosh = nodosh;
+        this->output = output;
 
-        // First layer 
+        // First layer
         matrix<double> init = getMatrix(input.size(), nodosh[0]);
         matrices.push_back(init);
 
+        std::cout << "inicializando matrices\n";
+
         // Intermediate layers
-        for (int i = 1; i < nodosh.size()-1; ++i)
+        
+        for (int i = 1; i < nodosh.size(); ++i)
         {
+            std::cout << "i: " << i << "\n";
             matrix<double> tmp = getMatrix(nodosh[i - 1], nodosh[i]);
             matrices.push_back(tmp);
         }
 
+        std::cout << "intermediate layers done\n";
+
         // Output Layer
-        matrix<double> moutput = getMatrix(nodosh[nodosh.size()-1],output);
+        std::cout << "prueba " << nodosh[nodosh.size() - 1] << "\n";
+        matrix<double> moutput = getMatrix(3,3);
         matrices.push_back(moutput);
-        
-    }
+        // CCERRO LA CLASE O ME PARECExdxd
+        // Ya no va hacer nada, cheqyen el grup
+        // xd
+        // estoy intentando volver a entrar pero nada jajaja xdxdxd
+        // QUiere que hacemos autodidactas :(
+        // no se si sienten lo mismo que yo, pero siento que el profe manda y manda y manda y manda cosas jajaj
+        // me estoy sofocando csm
+        // si fuera dos cusos que llevo normal pero estamos sofocadosxdxd
 
-    void calculate() {
-        int niveles_h = nodosh.size();
-        matrix<double> hk_prev(1, input.size());
-        //matrices.push_back(hk_prev);
-        // ingresar a hk_prev el input.
-        for (int i = 0; i <= niveles_h; ++i)
-            hk_prev(0, i) = input[i];
-            //neta por nivel
-            matrix<double> hk_prev = this->forward(hk_prev, matrices[i]);
-            //funcion de activcacion.
-            std::transform(hk_prev.begin1(),hk_prev.begin2(),logistic);
-            //se actualiza el input.
-            hk = matrices[i]
-            // matrices[]
-            // hk_prev = hk;
-            // hk = result;
-        }
-    }
+        // y lo tenemos en tesis tmb c
+        // F :( xxcxdxdxdxd
+        //sube cambios reneato como esta
 
 
-    double logistic(double x){
-        return 1 / (1 + exp(-x));
+                // pt llegar al punto de hacer catarsis por los comentarios de un archivo de C++. ahi te das cuenta que estas cagado
+        print("xd")
+        std::cout << "constructor ok\n";
     }
-    
-    
-    matrix<double> forward(matrix<double> hk_prev, matrix<double> hk)
+
+    std::vector<double> forward()
     {
-        return prod(hk_prev, hk);
+        int niveles_h = nodosh.size();
+        std::vector<double> hk_prev(input.size());
+        for (int i =0; i<  input.size();i++){
+             hk_prev[i] = input[i];
+        }
+        print(hk_prev);
+        // for (int i = 0; i < matrices.size(); ++i)
+        // {
+        //     hk_prev = this->calculate(hk_prev, matrices[i]);
+        //     for(int j=0; j<hk_prev.size();j++){
+        //         hk_prev[j]=logistic(hk_prev[j]);
+        //     }
+        //     this->print(hk_prev);
+        // }
+
+        return hk_prev;
     }
 
-    void backward() {
+    double logistic(double x)
+    {
+        return 1 / (1 + std::exp(-x));
+    }
+
+    std::vector<double> calculate(std::vector<double> hk_prev, matrix<double> hk)
+    {
+        matrix<double> hk_prev_to_matrix(1, hk_prev.size());
+        for (int i = 0; i < hk_prev.size(); ++i) {
+            hk_prev_to_matrix(0, i) = hk_prev[i];
+        }
+        matrix<double> result = prod(hk_prev_to_matrix, hk);
+        std::vector<double> vector_result(hk.size2(), 0);
+        for (int i = 0; i < hk.size2(); ++i) {
+            vector_result[i] = result(0, i);
+        }
+        return vector_result;
+    }
+
+    void backward()
+    {
 
     }
 
-}
-
-
-// void net(std::vector<int> input, std::vector<int> nodosh, int output){
-//     // 1 * len(input) para los x's del input
-//     // len(input) * nodosh[0]
-//     // net_1 = I * W^(Ih) + b^(Ih)
-// }
+    void print(std::vector<double> item){
+        for(auto i: item) std::cout<< i <<" ";
+    }
+    
+};
 
 int main()
 {
-   
-    // matrix<double> m(3, 3);
-    //  std::vector<matrix<double>> m;
-    // vector<matrix<double>> m3;
-    // matrix<double> m2(1, 3);
-    // for (unsigned i = 0; i < m.size1(); ++i)
-    //     for (unsigned j = 0; j < m.size2(); ++j)
-    //         m(i, j) = 3 * i + j;
+    std::vector<double> input{1, 2, 3, 4};
+    std::vector<int> nodosh{2, 3};
+    int output = 3;
 
-    // for (unsigned i = 0; i < m2.size1(); ++i)
-    //     for (unsigned j = 0; j < m2.size2(); ++j)
-    //         m2(i, j) = 3 * i + j;
+    std::cout << "creando objeto mlp\n";
 
-    // std::cout << m << std::endl;
-    // std::cout << m2 << std::endl;
-    // matrix<double> C;
-    // C = prod(m2,m);
-    // std::cout<<C;
+    MLP mlp = MLP(input, nodosh, output);
+    // auto tmp =mlp.forward();
     return 0;
 }
